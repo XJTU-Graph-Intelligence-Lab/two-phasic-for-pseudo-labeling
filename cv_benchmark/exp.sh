f821fe5289bd5b29632809abbea0c0e51fdcfe68
liyu199809 <1293384713@qq.com>
2024-11-21 21:35:55 +0800

    rm

--- cv_benchmark/exp.sh
+++
@@ -1,5 +0,0 @@
-python3 train.py --c config/two_phase/fixmatch/cifar_base200.yaml
-
-python3 train.py --c config/two_phase/pseudolabel/cifar_base200.yaml
-
-# python3 train.py --c config/usb_cv/adamatch/adamatch_cifar100_400_0_ns.yaml
